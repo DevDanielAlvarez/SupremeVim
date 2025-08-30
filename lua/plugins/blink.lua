2b8161c357ea5002a4e4594b257c3146ec008fff
Daniel Alvarez <skyrimdaniel11@gmail.com>
2025-08-30 20:46:01 -0300

    feat: add mini icons to blink

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,6 +1,9 @@
 return {
   'saghen/blink.cmp',
-  dependencies = { 'rafamadriz/friendly-snippets' },
+  dependencies = { 
+	  'rafamadriz/friendly-snippets',
+	  'nvim-mini/mini.icons'
+  },
   version = '1.*',
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
@@ -14,7 +17,30 @@ return {
     completion = {
 	    documentation = { auto_show = false },
 	    menu = {
-		    border = 'single'
+		    border = 'single',
+		    draw = {
+			padding = {1,2},
+			components = {
+        kind_icon = {
+          text = function(ctx)
+            local kind_icon, _, _ = require('mini.icons').get('lsp', ctx.kind)
+            return kind_icon
+          end,
+          -- (optional) use highlights from mini.icons
+          highlight = function(ctx)
+            local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+            return hl
+          end,
+        },
+        kind = {
+          -- (optional) use highlights from mini.icons
+          highlight = function(ctx)
+            local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+            return hl
+          end,
+        }
+      }
+		    }
 	    }
     },
 
