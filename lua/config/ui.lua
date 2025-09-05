ef297e9127e9492278b5fdd3f540abe2c832947c
Daniel Alvarez <skyrimdaniel11@gmail.com>
2025-09-05 19:34:27 -0300

    feat: add which keys from folke :D

--- lua/config/ui.lua
+++ lua/config/ui.lua
@@ -3,3 +3,8 @@ local vimOpt = vim.opt
 vimOpt.number = true
 vimOpt.relativenumber = true
 
+--TAB
+vim.opt.expandtab = true -- Converte TAB em espaços
+vim.opt.tabstop = 4      -- Mostra a largura de um TAB como 4 espaços
+vim.opt.shiftwidth = 4   -- Número de espaços usados para cada nível de indentação
+vim.opt.softtabstop = 4  -- Número de espaços que um <Tab> insere no modo Insert
